0022b7075cde9e568719138a36a8c5c7a52203d0
KambarManjunath <115543358+KambarManjunath@users.noreply.github.com>
2023-01-17 12:41:57 +0530

    Update EmailMsghandler.c

--- EmailMsghandler.c
+++ EmailMsghandler.c
@@ -15,23 +15,23 @@ StatusType sendToEmail(BreachType breachType)
 
 StatusType sendNormalMessage(const char* recepient)
 {
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is Normal\n");
+      //printf("To: %s\n", recepient);
+      //printf("Hi, the temperature is Normal\n");
 	  return SEND_TO_EMAIL_NORMAL;
 }
 
 StatusType sendTooLowMessage(const char* recepient)
 {
-	  printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
+      //printf("To: %s\n", recepient);
+      //printf("Hi, the temperature is too low\n");
 	  return SEND_TO_EMAIL_TOO_LOW;
 
 }
 
 StatusType sendTooHighMessage(const char* recepient)
 {
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
+      //printf("To: %s\n", recepient);
+      //printf("Hi, the temperature is too high\n");
 	  return SEND_TO_EMAIL_TOO_HIGH;
 }
 
